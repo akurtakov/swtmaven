bbf959595a7f323af03e28fb86ff08bd7db57f0b
Aleksandar Kurtakov <akurtakov@gmail.com>
2025-05-27 13:33:44 +0300

    Initial content

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# swtmaven
+# Build
+
+Use `mvn clean verify -Dosgi.platform=YOUR_WS.YOUR_OS.YOUR_ARCH` e.g. `mvn clean verify -Dosgi.platform=gtk.linux.x86_64`
+
+# Run the app via Maven
+
+mvn exec:java -Dexec.mainClass="com.akurtakov.App" -Dosgi.platform=gtk.linux.x86_64
---
+++ src/main/java/com/akurtakov/App.java
@@ -0,0 +1,17 @@
+package com.akurtakov;
+
+import org.eclipse.swt.widgets.*;
+
+public class App {
+
+public static void main (String [] args) {
+	Display display = new Display ();
+	Shell shell = new Shell(display);
+	shell.setText("Demo");
+	shell.open ();
+	while (!shell.isDisposed ()) {
+		if (!display.readAndDispatch ()) display.sleep ();
+	}
+	display.dispose ();
+}
+}
